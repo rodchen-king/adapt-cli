d02f85578bf7aa4180543baff7a304a5b869d093
rodchen <rodchen_king@outlook.com>
2021-12-03 19:48:29 +0800

    1. init project

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: rodchen
+ * @Date: 2021-12-03 19:46:34
+ * @LastEditTime: 2021-12-03 19:47:51
+ * @LastEditors: rodchen
+ */
+#!/usr/bin/env node
+
+console.log('apapt design pro')
