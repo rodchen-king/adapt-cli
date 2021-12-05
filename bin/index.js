3631a6b4a14e912935534bb52cc2ecd692a58bad
rodchen <rodchen_king@outlook.com>
2021-12-05 17:48:23 +0800

    1. yargs基本用法

--- bin/index.js
+++ bin/index.js
@@ -1,34 +1,38 @@
 #!/usr/bin/env node
 
-const lib = require('adapt-lib');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers');
+const dedent = require('dedent')
 
-console.log('welcome adapt design pro')
+const argv = hideBin(process.argv);
 
-const argv = require('process').argv;
+const cli = yargs(argv);
 
-// 第三个元素是command
-const command = argv[2]
+cli
+  .usage('Usage: adapt-cli [command] <options>')  // usage tip
+  .demandCommand(1, "A command is required. Pass --help to see all available commands and options.") // 最少传入的command数量
+  .strict()
+  .alias("h", "help")  // 别名
+  .alias("v", "version")
+  .wrap(cli.terminalWidth())  // 命令的宽度 cli.terminalWidth 占满
+  .epilogue(dedent` 
+      When a command fails, all logs are written to lerna-debug.log in the current working directory.
 
-// 第四个元素是options
-const options = argv.slice(3)
-
-if (options.length > 1) {
-  let [option, param] = options;
-  option = option.replace('--', '')
-  
-  if (lib[command]) {
-    lib[command](option, param)
-  } else {
-    console.log("请输入命令")
-  }
-}
-
-// 实现全局参数解析--version
-if (command.startsWith('--') || command.startsWith('-')) {
-  const globalOption = command.replace(/--|-/g, '');
-
-  if (globalOption == 'version' || globalOption == 'V') {
-    console.log('1.0.0')
-  }
-}
-
+      For more information, find our manual at https://github.com/lerna/lerna
+    `) // dedent不需要缩进
+  .options({
+     debug: {
+       type: 'boolean',
+       describe: 'bootstrap debug mode',
+       alias: 'd'
+     }
+  })
+  .options('registry', {
+    type: 'boolean',
+    // hidden: true
+    describe: 'define global registry',
+    alias: 'r'
+  })
+  .group(['debug'], 'Dev Options') // 分组
+  .group(['registry'], 'Publish Options') // 分组
+  .argv;
